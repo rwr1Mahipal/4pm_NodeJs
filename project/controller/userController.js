3caa7c27de09fc0a55609f6050e15892e2d88b58
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-03-13 16:30:50 +0530

    Add files via upload

--- project/controller/userController.js
+++ project/controller/userController.js
@@ -1,4 +1,5 @@
 const User = require("../model/userModel");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
   try {
@@ -8,7 +9,16 @@ exports.register = async (req, res) => {
       return res.status(401).json({ message: "All filds  are required" });
     }
 
-    const newUser = await User.create({ name, email, password });
+    const exiUser = await User.findOne({ email });
+    if (exiUser) {
+      return res.status(401).json({ message: "User is already register" });
+    }
+    const hasPass = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ 
+      name, 
+      email, 
+      password: hasPass 
+    });
     return res
       .status(201)
       .json({ message: "Registration successfull", newUser });
